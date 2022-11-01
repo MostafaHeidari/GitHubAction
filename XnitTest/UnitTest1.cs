16dcd049d61adfb7c9f74993718c8cac3ebfe659
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-11-01 20:19:50 +0100

    push2

--- XnitTest/UnitTest1.cs
+++ XnitTest/UnitTest1.cs
@@ -6,7 +6,7 @@ using Xunit.Sdk;
 //t
 namespace XUnitTest
 {
-    // createProduct b 1
+    // createProduct new
     public class UnitTest1
     {
         //instance variable used just for Multiply test method
